e69c25c1bdc235bfa7482e6052d8fdb4054e384c
Tom Landau <tomlandau@gmail.com>
2017-09-19 16:19:04 +0300

    jest tester - update code from bitsrc component, handle general test suite failures

--- src/testers/jest.js
+++ src/testers/jest.js
@@ -18,7 +18,10 @@
 const jest = require('jest');
 const sinon = require('sinon');
 const TestUtils = require('react-dom/test-utils');
+const React = require('react');
 const ReactDom = require('react-dom');
+const path = require('path');
+const fs = require('fs');
 const isEmptyObject = obj => Object.keys(obj).length === 0;
 const exec = require('child-process-promise').exec;
 
@@ -34,36 +37,69 @@ function mockDom(markup) {
   };
 }
 
-const normalizeResults = (stdout) => {
-  const results = JSON.parse(stdout)
+const normalizeResults = (results) => {
   const testResults = results.testResults;
+  let failures = [];
   const res = testResults.map(test=> {
     const duration = test.endTime - test.startTime
-    const testProps = test.assertionResults.map(assertionRes => {
-      const title = assertionRes.title;
-      const pass = (assertionRes.status === 'passed') ? true : false;
-      const err = (!pass) ? {  message: assertionRes.failureMessages[0] , stack: assertionRes.failureMessages[0] } : undefined;
-      if (err) return {title, pass, duration, err}
-      return {title, pass, duration}
-    });
+
+    if (isEmptyObject(test.assertionResults)) {
+      failures.push({
+        title: 'Test suite failed to run',
+        err: test.message,
+        duration: duration
+      });
+    } else {
+      const testProps = test.assertionResults.map(assertionRes => {
+        const title = assertionRes.title;
+        const pass = (assertionRes.status === 'passed') ? true : false;
+        const err = (!pass) ? {  message: assertionRes.failureMessages[0] , stack: assertionRes.failureMessages[0] } : undefined;
+        if (err) return {title, pass, duration, err}
+        return {title, pass, duration}
+      });
+    }
+
     const StatsProps = {
       start: test.startTime,
       end: test.endTime,
       duration: duration
     }
+
     const pass = (test.status === 'passed') ? true : false;
+    
     return {tests: testProps, stats: StatsProps, pass};
   });
 
   return res[0];
 }
+
+const readResults = (filePath = 'results.json') => {
+  const results = fs.readFileSync(filePath);
+  const parsedResults = JSON.parse(results);
+  fs.unlinkSync(filePath);
+  return parsedResults;
+}
+
 const run = (specFile) => {
-  return exec(`node ${__dirname}/node_modules/.bin/jest ${specFile} --json`).then(({err, stdout, stderr}) => {
-    return parseResults(stdout);
+    const resultsFilePath = `${extractFileNameFromPath(specFile)}-results.json`;
+    const jestPath = path.resolve(__dirname, '../../', 'node_modules/.bin/jest');
+    // We are using outputFile flag because in some cases when using --json only
+    // There is not valid json return, see details here:
+    // https://github.com/facebook/jest/issues/4399
+    const cmd = `node ${jestPath} ${specFile} --json --outputFile="${resultsFilePath}"`;
+  return exec(cmd).then(({err, stdout, stderr}) => {
+    const parsedResults = readResults(resultsFilePath);
+    return normalizeResults(parsedResults);
   }).catch(({message, stdout, stderr}) =>{
-    return parseResults(stdout);
+    const parsedResults = readResults(resultsFilePath);
+    return normalizeResults(parsedResults);
   });
 }
+
+const extractFileNameFromPath = (filePath) => {
+  let fileName = filePath.split('/').pop();
+  return fileName.split('.')[0];
+};
 
 module.exports = {
   run,
