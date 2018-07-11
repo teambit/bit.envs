7c3274f0b37198506dcf9fb929e3347c75cdf3f9
tomlandau <tomlandau@gmail.com>
2018-07-11 16:19:30 +0300

    typescript compiler - dont compile d.ts files

--- src/compilers/typescript/index.js
+++ src/compilers/typescript/index.js
@@ -11,12 +11,12 @@ const compileSingleFile = (file, compilerOptions, distPath) => {
   const mappings = new Vinyl({
     contents: new Buffer(result.sourceMapText),
     base: distPath,
-    path: _getDistFilePath(file, distPath),
+    path: _getDistFilePath(file, distPath, true),
   });
   mappings.basename = _getRevisedFileExtension(file.basename) + '.map';
 
   const fileContent = result.outputText ?  new Buffer(`${result.outputText}\n\n//# sourceMappingURL=${result.sourceMapText}`) : new Buffer(result.outputText);
-  const distFile = _getDistFile(file, distPath, fileContent);
+  const distFile = _getDistFile(file, distPath, true, fileContent);
 
   return [mappings,distFile];
 };
@@ -32,19 +32,19 @@ const compile = (files, distPath) => {
   const filesByToCompile = groupBy(files, _toCompile);
   
   const compiled = (!filesByToCompile.true || filesByToCompile.true.length === 0) ? [] : filesByToCompile.true.map(file => compileSingleFile(file, compilerOptions, distPath)).reduce((a, b) => a.concat(b));
-  const nonCompiled = !filesByToCompile.false ? [] : filesByToCompile.false.map(file => _getDistFile(file, distPath));
+  const nonCompiled = !filesByToCompile.false ? [] : filesByToCompile.false.map(file => _getDistFile(file, distPath, false));
 
   return compiled.concat(nonCompiled);;
 }
 
 const _toCompile = (file) => {
-  return compiledFileTypes.indexOf(file.extname.replace('.','')) > -1;
+  return compiledFileTypes.indexOf(file.extname.replace('.','')) > -1 && !file.stem.endsWith('.d');
 }
 
-const _getDistFile = (file, distPath, content) => {
+const _getDistFile = (file, distPath, reviseExtension, content) => {
   let distFile = file.clone();
   distFile.base = distPath;
-  distFile.path = _getDistFilePath(file, distPath);
+  distFile.path = _getDistFilePath(file, distPath, reviseExtension);
 
   if (content) {
     distFile.contents = content;
@@ -53,8 +53,12 @@ const _getDistFile = (file, distPath, content) => {
   return distFile;
 }
 
-const _getDistFilePath = (file, distPath) => {
-  return path.join(distPath, _getRevisedFileExtension(file.relative)); 
+const _getDistFilePath = (file, distPath, reviseExtension) => {
+  var fileRelative = file.relative;
+  
+  if (reviseExtension) fileRelative = _getRevisedFileExtension(file.relative);
+
+  return _path2.default.join(distPath, fileRelative);
 }
 
 const _getRevisedFileExtension = (fileName) => {
