e42b1a1f38f4da3e94d24431e23b2329b5a869fb
Gilad Shoham <shoham.gilad@gmail.com>
2019-05-07 17:26:08 +0300

    update script to use production bit version

--- src/scripts/import.sh
+++ src/scripts/import.sh
@@ -3,7 +3,7 @@ if [ -z "$scope" ]
 then
       echo "please provide scope"
 else
-      components=$(bd ls ${scope} -j | jq -r '.[].id')
+      components=$(bit ls ${scope} -j | jq -r '.[].id')
       num_components=$(echo "$components" | wc -w | xargs)
       echo "found $num_components in scope $scope"
       bit import $components
