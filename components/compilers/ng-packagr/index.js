aa3a0da12d3526faf342f739cd5fa1d4ff6c98b3
Doron Tsur <qballer@gmail.com>
2019-06-12 15:14:48 +0300

    before setting dist

--- components/compilers/ng-packagr/index.js
+++ components/compilers/ng-packagr/index.js
@@ -6,7 +6,10 @@ import 'ng-packagr'
 
 import path from 'path'
 import {default as execa} from 'execa'
+
 import {writeFileSync, readFileSync} from 'fs';
+// import {writeFileSync, readFileSync, unlinkSync} from 'fs';
+
 import readdir from 'recursive-readdir'
 import Vinyl from 'vinyl';
 
@@ -16,36 +19,48 @@ const compile = async (files, distPath, context) => {
     const mainFile = context.componentObject.mainFile
     const uuidHack = `capsule-${Date.now().toString().slice(-5)}`
     const directory = path.join(os.tmpdir(), uuidHack );
-    console.log('\ncompiling in dir: ', directory)
-    await execa(
-       'bd', ['isolate', 
-                '-d', directory, 
-                '-w', 'true', 
-                context.componentObject.name, 
-                '--use-capsule']
-    )
-    debugger
-    const dependencies = Object.keys(context.componentObject.dependencies).concat(Object.keys(context.componentObject.packageDependencies))
-    const ngPackge = createPackagrFile(directory, {main: mainFile, dist: distPath, name:context.componentObject.name, dependencies})
+    console.log('ahoy')
+    // await execa(
+    //    'bd', ['isolate', 
+    //             '-d', directory, 
+    //             '-w', 'true', 
+    //             context.componentObject.name, 
+    //             '--use-capsule']
+    // )
+
+    const res = await context.isolate(directory)
+    const capsule = res.capsule
+    
+    const dependencies = Object
+                            .keys(context.componentObject.dependencies)
+                            .concat(Object.keys(context.componentObject.packageDependencies))
+    const info = {
+        main: mainFile,
+        dist: distPath, 
+        name:context.componentObject.name, 
+        dependencies,
+        capsule
+    }
+    const ngPackge = createPackagrFile(directory, info)
     await runNGPackagr(ngPackge)
-    const compiledFiles = await collectDistFiles(directory, context.componentObject.name)
-    debugger
-    //delete
+    const compiledFiles = await collectDistFiles(directory, info)
+    capsule.destroy()
+    const mainDistFile = path.join('dist', info.name,'esm2015', path.basename(mainFile))
+    console.log('\nmain dist file: ', mainDistFile)
     return compiledFiles
 }
 
-export async function collectDistFiles(directory, name) {
-    const capsuleDir = getFullCapsuleDir(directory, name)
+export async function collectDistFiles(directory, info) {
+    const capsuleDir = getFullCapsuleDir(directory, info.name)
     
     const compDistDir = path.resolve(capsuleDir, 'dist')
     const files = await readdir(compDistDir)
     return files.map((file) => { 
         return new Vinyl({
-            path: path.join(`${name}`, file.split(path.join(capsuleDir, 'dist'))[1]),
+            path: path.join(`${info.name}`, file.split(path.join(capsuleDir, 'dist'))[1]),
             contents: readFileSync(file)
         })
     })
-
 }
 
 async function runNGPackagr(ngPackge) {
@@ -54,33 +69,28 @@ async function runNGPackagr(ngPackge) {
     try {
         result = await execa('node', [scriptFile, '-p', ngPackge])
     } catch (e) {
-        console.log('error:', e)
+        console.log('Error in packaging component:', e)
+        throw e
     }
-    // console.log('res', result.stdout)
-    // consider this location in order to collect diagnostics
     return result
 }
 
 function createPackagrFile(capsuleDir, info) {
     const compDir = getFullCapsuleDir(capsuleDir, info.name)
-    console.log('comp-dir: ', compDir)
-    // const fileName = info.main.split('/')[info.main.split('/').length -1 ]
-    const content = `
-        {
-            "$schema": "https://raw.githubusercontent.com/ng-packagr/ng-packagr/master/src/ng-package.schema.json",
-            "dest": "dist",
-            "lib": {
-                "entryFile": "${info.main}"
-            },
-            "whitelistedNonPeerDependencies":[${info.dependencies.map(val => `"${val}"`)}]
-        }
-    `
+    const content = `{
+        "$schema": "https://raw.githubusercontent.com/ng-packagr/ng-packagr/master/src/ng-package.schema.json",
+        "dest": "dist",
+        "lib": {
+            "entryFile": "${info.main}"
+        },
+        "whitelistedNonPeerDependencies":[${info.dependencies.map(val => `"${val}"`)}]
+    }`
     const filePath = path.resolve(path.join(compDir, 'ng-package.json'))
     writeFileSync(filePath, content)
     return filePath
 }
 
-export function getFullCapsuleDir(capsuleDir, name) {
+function getFullCapsuleDir(capsuleDir, name) {
     return path.join(capsuleDir, 'components', name) 
 }
 
